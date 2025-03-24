0660b0934978441ce5e0f77df5038800d15f418b
MashaT453 <mtrezune@gmail.com>
2025-03-24 15:41:51 +0200

    Update a.py

--- a.py
+++ a.py
@@ -1,2 +1,12 @@
 print("adasasd")
 print("helllo")
+
+class Persona:
+    def __init__(self, vards, vecums): # konstruktora definēšana
+        self.vards = vards # šeit tiek izveidots lauks vards
+        self.vecums = vecums # šeit tiek izveidots lauks vecums
+    def drukat(self): # metodes definēšana
+        print(self.vards, self.vecums)
+persona = Persona("Pēteris ", 20) # objekta izveidošana un konstruktora izsaukšana
+persona.drukat()
+# Pēteris 20 
